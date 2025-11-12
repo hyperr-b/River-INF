9f6df10236ad78b856409682c8c141e125f5b55f
qegf <matiaslagemann@gmail.com>
2025-11-11 22:08:17 -0300

    tentativa de mover aviao e tiro

---
+++ movimento.h
@@ -0,0 +1,9 @@
+#ifndef MOVIMENTO_H
+#define MOVIMENTO_H
+
+void moveteclado_aviao(float *aviao_x);
+void move_constante_aviao(float *aviao_y);
+void tiro_origem(float *tiro_y, float *tiro_x, float *aviao_x, float *aviao_y);
+int tiro_movimento(float *tiro_y);
+#endif
+
